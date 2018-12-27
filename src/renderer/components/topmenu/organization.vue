141a7a30d1ab1f07a3e712c0ba703d295dec0003
Sumedh Nimkarde <blesssumedh@gmail.com>
2018-12-27 16:26:22 +0530

    :art: Integrate the removeUserFromOrganization API

--- src/renderer/components/topmenu/organization.vue
+++ src/renderer/components/topmenu/organization.vue
@@ -9,10 +9,13 @@
 			<div class="ui header">
 				Organization Members
 			</div>
+			<div class="ui message" id="delete_member_message" style="display:none;">
+				<p></p>
+			</div>
 			<div v-for="(member, index) in members" v-bind:key="index" class="ui segment">
 				<div>
 					{{ member.first_name }} {{ member.last_name }}
-					<span><button class="ui mini red button right floated">remove</button></span>
+					<span><button class="ui mini red button right floated" v-on:click="removeUserFromOrganization(member)">remove</button></span>
 				</div>
 			</div>
 		</div>
@@ -35,7 +38,6 @@ export default {
 	},
 	async mounted() {
 		this.members = await this.getOrganizationMembers();
-
 	},
 	data() {
 		return {
@@ -49,9 +51,7 @@ export default {
 				headers: { "x-access-token": store.get("x-access-token") }
 			};
 			const result = await axios.get(
-				`${
-					process.env.API_BASE
-				}/organizations/${organizationId}/members`,
+				`${process.env.API_BASE}/organizations/${organizationId}/members`,
 				config
 			);
 			const { data: resultData } = result;
@@ -59,6 +59,52 @@ export default {
 				return resultData.data;
 			}
 			return [];
+		},
+		async removeUserFromOrganization(member) {
+			const organizationId = this.$route.params.uuid;
+			const config = {
+				headers: { "x-access-token": store.get("x-access-token") }
+			};
+			const data = {
+				user_id: member.uuid,
+				organization_id: organizationId
+			};
+			const result = await axios({
+				url: `${process.env.API_BASE}/status/remove`,
+				data,
+				method: "delete",
+				headers: config.headers
+			});
+			const { data: resultData } = result;
+
+			if (resultData.success) {
+				$("#delete_member_message")
+					.addClass("success")
+					.css("display", "block");
+				$("#delete_member_message p").html(resultData.message);
+
+				// updating the binded model.
+				this.members = await this.getOrganizationMembers(organizationId);
+
+				// success message for 2s.
+				setTimeout(() => {
+					$("#delete_member_message")
+						.removeClass("success")
+						.css("display", "none");
+				}, 2000);
+			} else {
+				$("#delete_member_message")
+					.addClass("error")
+					.css("display", "block");
+				$("#delete_member_message p").html(resultData.message);
+
+				// error message for 2s.
+				setTimeout(() => {
+					$("#delete_member_message")
+						.removeClass("errors")
+						.css("display", "none");
+				}, 2000);
+			}
 		}
 	}
 };
